027badc8859f2b7a16413151453dabef6941af69
Mohammed <117067006+Mehmedx7@users.noreply.github.com>
2023-07-23 13:37:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,46 +2,61 @@
 const csvFilePath = 'airports.csv';
 let airportsData = [];
 const departureInput = document.getElementById('departureInput');
+const airportList = document.getElementById('airportList');
 
-async function readCSV() {
-    try {
-        const response = await fetch(csvFilePath);
-        const data = await response.text();
-        airportsData = parseCSV(data);
-        initAutocomplete();
-        console.log('CSV data:', airportsData);
-    } catch (error) {
-        console.error('Error fetching CSV data:', error);
-    }
+function fetchCSV() {
+    fetch(csvFilePath)
+        .then(response => response.text())
+        .then(data => {
+            airportsData = Papa.parse(data, { header: true }).data;
+            initFuse();
+        })
+        .catch(error => console.error('Error fetching CSV file:', error));
 }
 
-function parseCSV(csvData) {
-    const lines = csvData.split(/\r?\n/);
-    const headers = lines[0].split(',');
-    const airports = [];
+function initFuse() {
+    const fuseOptions = {
+        keys: ['code', 'name'], // Properties to search for (code and name in this case)
+        threshold: 0.3, // Adjust this value to control the sensitivity of the search
+        shouldSort: true
+    };
 
-    for (let i = 1; i < lines.length; i++) {
-        const currentLine = lines[i].split(',');
-        if (currentLine.length === headers.length) {
-            const airport = {
-                code: currentLine[0].trim(),
-                name: currentLine[2].trim()
-            };
-            airports.push(airport);
-        }
-    }
-
-    return airports;
-}
-
-function initAutocomplete() {
-    const awesomplete = new Awesomplete(departureInput, {
-        list: airportsData.map(airport => `${airport.name} (${airport.code})`)
-    });
+    const fuse = new Fuse(airportsData, fuseOptions);
 
     departureInput.addEventListener('input', () => {
-        awesomplete.evaluate();
+        const inputValue = departureInput.value.trim();
+        if (inputValue.length >= 2) {
+            showAirportSuggestions(fuse.search(inputValue));
+        } else {
+            airportList.style.display = 'none';
+        }
+    });
+
+    departureInput.addEventListener('focus', () => {
+        showAirportSuggestions([]);
+    });
+
+    departureInput.addEventListener('blur', () => {
+        setTimeout(() => {
+            airportList.style.display = 'none';
+        }, 200);
     });
 }
 
-readCSV();
+function showAirportSuggestions(results) {
+    airportList.style.display = 'block';
+    airportList.innerHTML = '';
+    results.forEach(result => {
+        const airport = result.item; // Access the "item" property to get the airport object
+        const li = document.createElement('li');
+        li.textContent = `${airport.name} (${airport.code})`;
+        li.addEventListener('click', () => {
+            departureInput.value = `${airport.name} (${airport.code})`;
+            airportList.style.display = 'none';
+            console.log('Selected Airport:', airport);
+        });
+        airportList.appendChild(li);
+    });
+}
+
+fetchCSV();
